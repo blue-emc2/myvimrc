cf508e77acbdcfe284ba4e183fcf6e53cf310f36
Shunsuke Yamazaki <s.yamazaki@everyleaf.com>
2017-07-15 15:31:54 +0900

    update

--- config.fish
+++ config.fish
@@ -1,6 +1,11 @@
-# rbenv
-#eval "$(rbenv init -)";
+set fisher_home ~/.local/share/fisherman
+set fisher_config ~/.config/fisherman
+source $fisher_home/config.fish
+
+set zealot_right_prompt_off
+
 set -x PATH $HOME/.rbenv/bin $PATH
 set -x PATH $HOME/.rbenv/shims $PATH
 rbenv rehash >/dev/null ^&1
 
+export XDG_CONFIG_HOME="$HOME/.config"

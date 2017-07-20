d3e756d15f18785d97534f4ea6acab1f0a0b0bdc
Shunsuke Yamazaki <s.yamazaki@everyleaf.com>
2017-07-21 07:21:34 +0900

    update files

--- setup.sh
+++ setup.sh
@@ -5,9 +5,6 @@ ln -vsf ~/myvimrc/.vimrc ~/
 ln -vsf ~/myvimrc/.gitignore ~/
 ln -vsf ~/myvimrc/.gitconfig ~/
 #ln -vsf ~/myvimrc/.profile ~/
-ln -vsf ~/myvimrc/config.fish ~/.config/fish/
-
-# NeoBundle(vim)
-#mkdir -p ~/.vim/bundle
-#git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
+ln -vsf ~/myvimrc/config.fish ~/.config/fish
+ln -vsf ~/myvimrc/init.vim ~/.config/nvim/init.vim
 

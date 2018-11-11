8c19c2eca9483e33e77156d2d937784265c236be
Shunsuke Yamazaki <s.yamazaki@everyleaf.com>
2018-11-11 12:26:35 +0900

    Add direnv config.

--- config.fish
+++ config.fish
@@ -4,11 +4,10 @@ source $fisher_home/config.fish
 
 set zealot_right_prompt_off
 
-set -x PATH $HOME/.rbenv/bin $PATH
-set -x PATH $HOME/.rbenv/shims $PATH
-rbenv rehash >/dev/null ^&1
-
 set -x XDG_CONFIG_HOME $HOME/.config
 set -x XDG_CACHE_HOME $HOME/.cache
 
 alias vim=nvim
+
+set -x EDITOR vim $EDITOR
+eval (direnv hook fish)
